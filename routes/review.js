f649e915b1f816bc5e6caa029bfa8ea0b72f5bc0
Kunal <kunaltailor5555@gmail.com>
2024-11-18 23:33:55 +0530

    intial commit

--- routes/review.js
+++ routes/review.js
@@ -28,7 +28,7 @@ router.post(
 
     await newReview.save();
     await listing.save();
-
+    req.flash("success","New Review Created ! ")
     res.redirect(`/listings/${listing._id}`);
     // console.log("new review saved");
     // res.send("new review saved");
@@ -42,6 +42,7 @@ router.delete(
     let { id, reviewId } = req.params;
     await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
+    req.flash("success","Review Deleted ! ")
 
     res.redirect(`/listings/${id}`);
   })
